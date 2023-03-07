06ba8d30b2e22cfb89a1a422da84c798c0bb9ab9
Nirajan000 <dr.phillip078@gmail.com>
2023-03-07 16:48:28 +0545

    first javascript

---
+++ Swaping_Numbers.js
@@ -0,0 +1,36 @@
+
+let a=10;
+let b=20;
+
+//swaping values
+
+let c;
+c=a;
+a=b;
+b=c;
+
+console.log("The value of a is",a);
+console.log("The value of b is",b);
+
+
+
+
+
+
+
+
+
+function swap_elements(arr,a,b){
+[arr[a],arr[b]]=[arr[b],arr[a]]
+}
+arr=[10,20,30,40,50,60];
+swap_elements(arr,0,3);
+console.log("The array elements after swaping are:",arr);
+
+
+
+
+
+
+
+
---
+++ Variable.js
@@ -0,0 +1,4 @@
+let Length=10;
+let breadth=20;
+
+console.log("Area of Reactangle is:",Length*breadth);
